533d0504d5d9024ef05d824fa848967357d789f6
srikarthickk <srikarthickk.mca22@rvce.edu.in>
2024-05-09 21:27:54 +0530

    init

--- galera/self_healing.sh
+++ galera/self_healing.sh
@@ -1,5 +1,16 @@
 mapfile -t ip_array < hosts.txt
 
 for oneIp in "${ip_array[@]}"; do
-    ssh -q -o StrictHostKeyChecking=no ubuntu@"$oneIp" "sudo cat /var/lib/mysql/grastate.dat | grep 'safe_to_bootstrap' | awk '{print \$2}'"
+	result=$(ssh -q -o StrictHostKeyChecking=no ubuntu@"$oneIp" "sudo cat /var/lib/mysql/grastate.dat | grep 'safe_to_bootstrap' | awk '{print \$2}'")
+    if [ $result == 1 ]; then
+	initialHealing="$oneIp"
+    fi
+done
+
+# recover
+ssh -q -o StrictHostKeyChecking=no ubuntu@"$initialHealing" "sudo galera_new_cluster"
+for oneIp in "${ip_array[@]}"; do
+	if [ $oneIp != $initialHealing ]; then
+		ssh -q -o StrictHostKeyChecking=no ubuntu@"$oneIp" "sudo systemctl start mysqld.service"
+	fi
 done
