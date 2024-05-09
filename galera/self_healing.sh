3b18ba48f0f4218990ed9c9fe706714de10b07e7
srikarthickk <srikarthickk.mca22@rvce.edu.in>
2024-05-09 22:20:09 +0530

    update to handle fewer nodes failure than all nodes failure

--- galera/self_healing.sh
+++ galera/self_healing.sh
@@ -1,16 +1,32 @@
 mapfile -t ip_array < hosts.txt
 
+ip_array_length=${#ip_array[@]}
+inactive_nodes=()
+
+# get the count of failed nodes
 for oneIp in "${ip_array[@]}"; do
-	result=$(ssh -q -o StrictHostKeyChecking=no ubuntu@"$oneIp" "sudo cat /var/lib/mysql/grastate.dat | grep 'safe_to_bootstrap' | awk '{print \$2}'")
-    if [ $result == 1 ]; then
-	initialHealing="$oneIp"
+	result=$(ssh -q -o StrictHostKeyChecking=no ubuntu@"$oneIp" "sudo systemctl is-active mysqld.service")
+    if [[ $result == "failed" || $result == "inactive" || $result == "activating" ]]; then
+		inactive_nodes+=($oneIp)
     fi
+	echo $oneIp - $result
 done
 
-# recover
-ssh -q -o StrictHostKeyChecking=no ubuntu@"$initialHealing" "sudo galera_new_cluster"
-for oneIp in "${ip_array[@]}"; do
-	if [ $oneIp != $initialHealing ]; then
-		ssh -q -o StrictHostKeyChecking=no ubuntu@"$oneIp" "sudo systemctl start mysqld.service"
-	fi
+# if all the nodes are failed
+if [ $ip_array_length == ${#inactive_nodes[@]} ]; then
+	for oneIp in "${ip_array[@]}"; do
+		result=$(ssh -q -o StrictHostKeyChecking=no ubuntu@"$oneIp" "sudo cat /var/lib/mysql/grastate.dat | grep 'safe_to_bootstrap' | awk '{print \$2}'")
+		if [ $result == 1 ]; then
+			initialHealing="$oneIp"
+		fi
+	done
+
+	# recover from the node that has the highest galera cluster sequence number
+	ssh -q -o StrictHostKeyChecking=no ubuntu@"$initialHealing" "sudo galera_new_cluster"
+
+fi
+
+# recover only failed nodes
+for oneIp in "${inactive_nodes[@]}"; do
+	ssh -q -o StrictHostKeyChecking=no ubuntu@"$oneIp" "sudo systemctl start mysqld.service"
 done
